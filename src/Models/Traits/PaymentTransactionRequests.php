8444354e96f514f5bdf61365a78b595f306455bb
Robert Kujawa <robert.kujawa@proton.me>
2023-03-28 12:52:48 -0600

    Add PaymentTransactionRequests

---
+++ src/Models/Traits/PaymentTransactionRequests.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Payavel\Checkout\Models\Traits;
+
+trait PaymentTransactionRequests
+{
+    use ConfiguresPaymentGateway;
+
+    /**
+     * Fetch the transaction details from the provider.
+     *
+     * @return \Payavel\Checkout\PaymentResponse
+     */
+    public function fetch()
+    {
+        return $this->gateway->getTransaction($this);
+    }
+
+    /**
+     * Request the provider to void the transaction.
+     *
+     * @param array|mixed $data
+     * @return \Payavel\Checkout\PaymentResponse
+     */
+    public function void($data = [])
+    {
+        return $this->gateway->void($this, $data);
+    }
+
+    /**
+     * Request the provider to refund the transaction.
+     *
+     * @param array|mixed $data
+     * @return \Payavel\Checkout\PaymentResponse
+     */
+    public function refund($data = [])
+    {
+        return $this->gateway->refund($this, $data);
+    }
+}
