b608366b89bc9fffc771796011e432bb613506bf
Robert Kujawa <robert.kujawa@proton.me>
2023-05-23 14:44:14 -0600

    Reorganize existing config/database driver tests

--- tests/Unit/ConfigDriverTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Payavel\Checkout\Tests\Unit;
-
-class ConfigDriverTest extends TestPaymentGateway
-{
-    protected $driver = 'config';
-}
--- tests/Unit/DatabaseDriverTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Payavel\Checkout\Tests\Unit;
-
-class DatabaseDriverTest extends TestPaymentGateway
-{
-    protected $driver = 'database';
-}
