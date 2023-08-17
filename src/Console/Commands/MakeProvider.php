646f07b96c3993754c8c6f0a52380f85dea09692
Robert Kujawa <robert.kujawa@proton.me>
2023-08-17 16:47:48 -0600

    Reuse service:provider command

--- src/Console/Commands/MakeProvider.php
+++ src/Console/Commands/MakeProvider.php
@@ -3,14 +3,10 @@
 namespace Payavel\Checkout\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Str;
-use Payavel\Checkout\Traits\GeneratesFiles;
-use Payavel\Checkout\Traits\Questionable;
+use Illuminate\Support\Facades\Artisan;
 
 class MakeProvider extends Command
 {
-    use Questionable, GeneratesFiles;
-
     /**
      * The name and signature of the console command.
      *
@@ -29,63 +25,17 @@ class MakeProvider extends Command
     protected $description = 'Scaffold a new payment provider\'s gateway and response classes.';
 
     /**
-     * The payment provider attributes to be saved.
-     *
-     * @var string $name
-     * @var string $id
-     */
-    protected $name, $id;
-
-    /**
      * Execute the console command.
      *
      * @return void
      */
     public function handle()
     {
-        $this->setProperties();
-
-        $this->generateProvider();
-    }
-
-    /**
-     * Format the payment provider's properties.
-     *
-     * @return void
-     */
-    protected function setProperties()
-    {
-        if ($this->option('fake', false)) {
-            $this->name = 'Fake';
-            $this->id = 'fake';
-
-            return;
-        }
-
-        $this->name = trim($this->argument('provider') ?? $this->askName('provider'));
-
-        $this->id = $this->option('id') ?? $this->askId('provider', $this->name);
-    }
-
-    /**
-     * Generated the provider gateway files.
-     *
-     * @return void
-     */
-    protected function generateProvider()
-    {
-        $provider = Str::studly($this->id);
-
-        $this->putFile(
-            app_path("Services/Payment/{$provider}PaymentRequest.php"),
-            $this->makeFile(__DIR__ . '/../../stubs/payment-request.stub', ['name' => $provider])
-        );
-
-        $this->putFile(
-            app_path("Services/Payment/{$provider}PaymentResponse.php"),
-            $this->makeFile(__DIR__ . '/../../stubs/payment-response.stub', ['name' => $provider])
-        );
-
-        $this->info("{$this->name} payment gateway generated successfully!");
+        Artisan::call('service:provider', [
+            'provider' => $this->argument('provider'),
+            '--service' => 'checkout',
+            '--id' => $this->option('id'),
+            '--fake' => $this->option('fake', false),
+        ]);
     }
 }
