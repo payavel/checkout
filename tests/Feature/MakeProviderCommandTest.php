cc319c7bf3a1adb8b0cbcf4eb8c444c86c0bc3ac
Robert Kujawa <robert.kujawa@proton.me>
2023-08-24 13:12:12 -0600

    Test checkout:provider command

--- tests/Feature/MakeProviderCommandTest.php
+++ tests/Feature/MakeProviderCommandTest.php
@@ -2,71 +2,34 @@
 
 namespace Payavel\Checkout\Tests;
 
-use Illuminate\Support\Str;
-use Payavel\Serviceable\Models\Provider;
+use Payavel\Serviceable\Service;
+use Payavel\Serviceable\Tests\Traits\AssertGatewayExists;
+use Payavel\Serviceable\Tests\Traits\CreateServiceables;
 
 class MakeProviderCommandTest extends TestCase
 {
+    use AssertGatewayExists,
+        CreateServiceables;
+
     protected function getEnvironmentSetUp($app)
     {
         parent::getEnvironmentSetUp($app);
 
-        $app['config']->set('serviceable.services.checkout', [
-            'name' => 'Checkout',
-            'config' => 'payment',
-        ]);
+        $this->createService(['id' => 'checkout', 'name' => 'Checkout']);
     }
 
     /** @test */
-    public function make_payment_provider_command_will_prompt_for_missing_arguments()
+    public function make_provider_command_defaults_to_checkout_service()
     {
-        $provider = Provider::factory()->make();
-
-        $this->artisan('checkout:provider')
-            ->expectsQuestion('What checkout provider would you like to add?', $provider->name)
-            ->expectsQuestion("How would you like to identify the {$provider->name} checkout provider?", $provider->id)
-            ->expectsOutput("{$provider->name} checkout gateway generated successfully!")
-            ->assertExitCode(0);
-
-        $this->assertGatewayExists($provider->id);
-    }
-
-    /** @test */
-    public function make_payment_provider_command_completes_without_asking_questions_when_providing_the_arguments()
-    {
-        $provider = Provider::factory()->make();
+        $provider = $this->createProvider(Service::find('checkout'));
 
         $this->artisan('checkout:provider', [
-                'provider' => $provider->name,
-                '--id' => $provider->id,
-            ])
+            'provider' => $provider->getName(),
+            '--id' => $provider->getId(),
+        ])
             ->expectsOutput("{$provider->name} checkout gateway generated successfully!")
             ->assertExitCode(0);
 
-        $this->assertGatewayExists($provider->id);
-    }
-
-    /** @test */
-    public function make_payment_provider_command_with_fake_argument_generates_fake_gateway()
-    {
-        $arguments = [
-            '--fake' => true,
-        ];
-
-        $this->artisan('checkout:provider', $arguments)
-            ->expectsOutput('Fake checkout gateway generated successfully!')
-            ->assertExitCode(0);
-
-        $this->assertGatewayExists('fake');
-    }
-
-    private function assertGatewayExists(string $id)
-    {
-        $provider = Str::studly($id);
-
-        $servicePath = app_path('Services/Checkout');
-
-        $this->assertTrue(file_exists("{$servicePath}/{$provider}CheckoutRequest.php"));
-        $this->assertTrue(file_exists("{$servicePath}/{$provider}CheckoutResponse.php"));
+        $this->assertGatewayExists($provider);
     }
 }
