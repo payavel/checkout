d8d1691fdb6fcfc0d900afe65465e399b11c2475
r-kujawa <robert.kujawa@proton.me>
2024-04-14 14:08:27 -0600

    Move database out of src

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace Payavel\Checkout;
 
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Payavel\Checkout\Console\Commands\CheckoutInstall;
 use Payavel\Checkout\Console\Commands\CheckoutProvider;
-use Payavel\Orchestration\Service;
 
 class PaymentServiceProvider extends ServiceProvider
 {
@@ -38,7 +36,7 @@ class PaymentServiceProvider extends ServiceProvider
     protected function registerPublishableAssets()
     {
         $this->publishes([
-            __DIR__ . '/database/migrations/2021_01_01_000000_create_base_payment_tables.php' => database_path('migrations/2021_01_01_000000_create_base_payment_tables.php'),
+            __DIR__ . '/../database/migrations/2024_01_01_000010_create_base_checkout_tables.php' => database_path('migrations/2024_01_01_000010_create_base_checkout_tables.php'),
         ], 'payavel-migrations');
 
         $this->publishes([
