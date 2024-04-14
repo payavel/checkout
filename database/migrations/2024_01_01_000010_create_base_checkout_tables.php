0039751c77d8cb6bceedddba8c269ce901ffc581
r-kujawa <robert.kujawa@proton.me>
2024-04-14 14:08:27 -0600

    Move database out of src

--- database/migrations/2024_01_01_000010_create_base_checkout_tables.php
+++ database/migrations/2024_01_01_000010_create_base_checkout_tables.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Payavel\Orchestration\Support\ServiceConfig;
 
-class CreateBasePaymentTables extends Migration
+class CreateBaseCheckoutTables extends Migration
 {
     /**
      * Run the migrations.
