3887c8ecd4bd6868c7887d2381e6fdea9690fd26
r-kujawa <robert.kujawa@proton.me>
2024-04-17 17:39:06 -0600

    Update payment.php config

--- config/payment.php
+++ config/payment.php
@@ -2,32 +2,30 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Payment Test Mode
-    |--------------------------------------------------------------------------
-    |
-    | When set to true, it will pass the provider & account into the testing
-    | gateway so you can mock your requests as you wish. This is very
-    | useful when you are running tests in a CI/CD environment.
-    |
-    */
-    'test_mode' => false,
-
+    'name' => 'Checkout',
 
     /*
     |--------------------------------------------------------------------------
-    | Payment Mocking
+    | Checkout Test Mode
     |--------------------------------------------------------------------------
     |
-    | Here you can override the location and/or class name of your fake payment
-    | request & response classes. Also, feel free to add any additional config
-    | that may assist you in defining your mocked checkout responses.
+    | When set to true, the provider & account will be shared with the fake payment
+    | request so you can mock your responses as you wish. This is very useful for
+    | local & testing environments where a sandbox is limited or non-existent.
     |
     */
-    'mocking' => [
-        'request_class' => \App\Services\Payment\FakePaymentRequest::class,
-        'response_class' => \App\Services\Payment\FakePaymentResponse::class,
-    ],
+    'test_mode' => env('PAYMENT_TEST_MODE', false),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Checkout Testing
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to define the location of the fake payment
+    | request & response classes you would like to leverage when test_mode
+    | is set to true. Also, feel free to add any other settings here.
+    |
+    */
+    'test_gateway' => \App\Services\Payment\FakePaymentRequest::class,
 
 ];
