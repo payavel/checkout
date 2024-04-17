0cf9555af9fc29892d7c9248f4074a63900caefa
r-kujawa <robert.kujawa@proton.me>
2024-04-17 08:43:54 -0600

    Inherit from orchestration

--- src/PaymentResponse.php
+++ src/PaymentResponse.php
@@ -4,14 +4,10 @@ namespace Payavel\Checkout;
 
 use Payavel\Checkout\Contracts\PaymentResponder;
 use Payavel\Checkout\Traits\PaymentResponses;
-use Payavel\Orchestration\Contracts\Accountable;
-use Payavel\Orchestration\Contracts\Providable;
-use Payavel\Orchestration\Traits\SimulatesAttributes;
-use RuntimeException;
+use Payavel\Orchestration\ServiceResponse;
 
-abstract class PaymentResponse implements PaymentResponder
+abstract class PaymentResponse extends ServiceResponse implements PaymentResponder
 {
-    use SimulatesAttributes;
     use PaymentResponses;
 
     /**
@@ -37,144 +33,11 @@ abstract class PaymentResponse implements PaymentResponder
     ];
 
     /**
-     * Customize the response method names for your requests.
-     *
-     * @var array
-     */
-    protected $responseMethods = [];
-
-    /**
-     * The provider's raw response.
-     *
-     * @var mixed
-     */
-    protected $rawResponse;
-
-    /**
-     * Additional information needed to format the response.
-     *
-     * @var mixed
-     */
-    protected $additionalInformation;
-
-    /**
-     * The request method that returned this response.
-     *
-     * @var string
-     */
-    public $requestMethod;
-
-    /**
-     * The provider that the $request was made towards.
-     *
-     * @var \Payavel\Orchestration\Contracts\Providable
-     */
-    public $provider;
-
-    /**
-     * The account that was used to make the $request.
-     *
-     * @var \Payavel\Orchestration\Contracts\Accountable
-     */
-    public $account;
-
-    /**
-     * The expected formatted data based on the $request.
-     *
-     * @var mixed
-     */
-    private $data;
-
-    /**
-     * @param mixed $rawResponse
-     * @param mixed $additionalInformation
-     */
-    public function __construct($rawResponse, $additionalInformation = null)
-    {
-        $this->rawResponse = $rawResponse;
-        $this->additionalInformation = $additionalInformation;
-
-        $this->setUp();
-    }
-
-    /**
-     * Set up the response.
-     *
-     * @return void
-     */
-    protected function setUp()
-    {
-        //
-    }
-
-    /**
-     * Configure the response based on the request.
-     *
-     * @param string $requestMethod
-     * @param \Payavel\Orchestration\Contracts\Providable $provider
-     * @param \Payavel\Orchestration\Contracts\Accountable $account
-     * @return void
-     */
-    public function configure(string $requestMethod, Providable $provider, Accountable $account)
-    {
-        $this->requestMethod = $requestMethod;
-        $this->provider = $provider;
-        $this->account = $account;
-    }
-
-    /**
-     * Get the provider's raw response.
-     *
-     * @return mixed
-     */
-    public function getRawResponse()
-    {
-        return $this->rawResponse;
-    }
-
-    /**
-     * Alias for the getRawResponse function.
-     *
-     * @return mixed
-     */
-    public function getRaw()
-    {
-        return $this->getRawResponse();
-    }
-
-    /**
-     * Verify whether the request should be considered successful.
-     *
-     * @return bool
-     */
-    public function isSuccessful()
-    {
-        return in_array($this->getStatusCode(), $this->successStatuses);
-    }
-
-    /**
-     * Verify whether the request should be considered a failure.
-     *
-     * @return bool
-     */
-    public function isNotSuccessful()
-    {
-        return ! $this->isSuccessful();
-    }
-
-    /**
-     * Determines the status code based on the request's raw response.
-     *
-     * @return int
-     */
-    abstract public function getStatusCode();
-
-    /**
      * Get a string representation of the response's status.
      *
      * @return string|null
      */
-    public function getStatus()
+    public function getStatusMessage()
     {
         return PaymentStatus::get($this->getStatusCode());
     }
@@ -184,47 +47,8 @@ abstract class PaymentResponse implements PaymentResponder
      *
      * @return string|null
      */
-    public function getMessage()
+    public function getStatusDescription()
     {
         return PaymentStatus::getMessage($this->getStatusCode());
-    }
-
-    /**
-     * Get the formatted details based on the request that was made.
-     *
-     * @return array|mixed
-     *
-     * @throws \RuntimeException
-     */
-    public function getData()
-    {
-        if (! isset($this->data)) {
-            $this->data = $this->{$this->getResponseMethod()}();
-        }
-
-        return $this->data;
-    }
-
-    /**
-     * Get the response method that should be used to get the response's data.
-     *
-     * @return string
-     */
-    protected function getResponseMethod()
-    {
-        if (isset($this->requestMethod)) {
-            if (
-                array_key_exists($this->requestMethod, $this->responseMethods) &&
-                method_exists($this, $method = $this->responseMethods[$this->requestMethod])
-            ) {
-                return $method;
-            }
-
-            if (method_exists($this, $method = "{$this->requestMethod}Response")) {
-                return $method;
-            }
-        }
-
-        return 'response';
     }
 }
