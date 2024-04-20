7e8801968c137a60e2575e767d9262d6c8b17108
r-kujawa <robert.kujawa@proton.me>
2024-04-20 07:55:42 -0600

    Register checkout service stubs

--- src/CheckoutServiceProvider.php
+++ src/CheckoutServiceProvider.php
@@ -38,7 +38,12 @@ class CheckoutServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/../database/migrations/2024_01_01_000010_create_base_checkout_tables.php' => database_path('migrations/2024_01_01_000010_create_base_checkout_tables.php'),
-        ], ['payavel', 'payavel-checkout', 'payavel-migrations']);
+        ], ['payavel', 'payavel-checkout', 'payavel-migrations', 'payavel-checkout-migrations']);
+
+        $this->publishes([
+            __DIR__ . '/../stubs/checkout/service-requester.stub' => base_path('stubs/checkout/service-requester.stub'),
+            __DIR__ . '/../stubs/checkout/service-responder.stub' => base_path('stubs/checkout/service-responder.stub'),
+        ], ['payavel', 'payavel-checkout', 'payavel-stubs', 'payavel-checkout-stubs']);
     }
 
     protected function registerCommands()
