3f8f726b085c9655bc37f114ffe5e16579b6f762
Robert Kujawa <robert.kujawa@proton.me>
2024-06-09 15:18:46 -0600

    Move 2 tests

--- tests/Unit/Database/CheckoutGatewayTest.php
+++ tests/Unit/Database/CheckoutGatewayTest.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Payavel\Checkout\Tests\Unit;
+namespace Payavel\Checkout\Tests\Unit\Database;
 
+use Payavel\Checkout\Tests\Unit\TestCheckoutGateway;
 use Payavel\Orchestration\Tests\Traits\CreatesDatabaseServiceables;
 use Payavel\Orchestration\Tests\Traits\SetsDatabaseDriver;
 
-class DatabaseCheckoutGatewayTest extends TestCheckoutGateway
+class CheckoutGatewayTest extends TestCheckoutGateway
 {
     use CreatesDatabaseServiceables;
     use SetsDatabaseDriver;
