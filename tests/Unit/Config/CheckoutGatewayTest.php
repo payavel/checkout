caba4b2dd502171c52423113cfe736f0447f4f14
Robert Kujawa <robert.kujawa@proton.me>
2024-06-09 15:18:46 -0600

    Move 2 tests

--- tests/Unit/Config/CheckoutGatewayTest.php
+++ tests/Unit/Config/CheckoutGatewayTest.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Payavel\Checkout\Tests\Unit;
+namespace Payavel\Checkout\Tests\Unit\Config;
 
+use Payavel\Checkout\Tests\Unit\TestCheckoutGateway;
 use Payavel\Orchestration\Tests\Traits\CreatesConfigServiceables;
 use Payavel\Orchestration\Tests\Traits\SetsConfigDriver;
 
-class ConfigCheckoutGatewayTest extends TestCheckoutGateway
+class CheckoutGatewayTest extends TestCheckoutGateway
 {
     use CreatesConfigServiceables;
     use SetsConfigDriver;
